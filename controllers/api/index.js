9bb3a391c2a70eeb793c80c03e1bfcd6d635e9a8
Zachary Zaleski <83981829+kingcrowz@users.noreply.github.com>
2021-09-09 18:39:06 -0400

    api controller index content

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const companyRoutes = require('./companyRoutes');
+const reviewRoutes = require('./reviewRoutes');
 
+router.use('/users', userRoutes);
+router.use('/company', companyRoutes);
+router.use('/review', reviewRoutes);
+
+module.exports = router;
