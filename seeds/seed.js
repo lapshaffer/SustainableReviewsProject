f1dd668920dc011905e88f975eb77ba7a35a0b74
Zachary Zaleski <83981829+kingcrowz@users.noreply.github.com>
2021-09-09 18:57:25 -0400

    added bulk to seed file
    still need to create json files to house seed info

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,29 @@
+const sequelize = require('../config/connection');
+const { User, Review, Company } = require('../models');
 
+const userData = require('./userData.json');
+const reviewData = require('./reviewData.json');
+const companyData = require('./companyData.json');
+
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Company.bulkCreate(companyData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Review.bulkCreate(reviewData, {
+        individualHooks: true,
+        returning: true,
+    });
+    process.exit(0);
+};
+
+seedDatabase();
