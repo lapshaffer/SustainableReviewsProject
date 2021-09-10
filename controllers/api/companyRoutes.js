7b96c29ce156a8287a043bd0b955b676b134382d
Rachel Chin <rtc145@gmail.com>
2021-09-09 20:57:45 -0400

    Create a new company route is working :)

--- controllers/api/companyRoutes.js
+++ controllers/api/companyRoutes.js
@@ -3,14 +3,15 @@ const {User, Company, Review} = require('../../models');
 
 // CREATE NEW COMPANY
 router.post('/', async (req, res) => {
+    console.log("*****************************");
     try {
         if (!req.session.logged_in) {
             res.status(400).json("User is not logged in");
             return
         }
         req.body.user_id = req.session.user_id;
-        const newPost = await Post.create(req.body);
-        res.status(200).json(newPost);
+        const newCompany = await Company.create(req.body);
+        res.status(200).json(newCompany);
     }
     catch (err) {
         console.log(err);
