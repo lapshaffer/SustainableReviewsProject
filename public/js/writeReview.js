bf8dd2a815f5c66ace36c929fbf352194d337e40
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-09-11 10:19:24 -0400

    review fetch created; body set up

--- public/js/writeReview.js
+++ public/js/writeReview.js
@@ -2,5 +2,27 @@
 const submitReview = require('#submitReview');
 
 submitReview.addeventListener('click', async (event) => {
-    
+    event.preventDefault();
+
+    const title = document.querySelector('#title');
+    const content = document.querySelector('#content');
+    const date = document.querySelector('#date');
+    const companyId = document.querySelector('#companyId');
+    const rating = document.querySelector('#rating').value;
+
+    const review = await fetch('/api/reviews', {
+        method: 'POST',
+        body: {
+            title: title,
+            content: content,
+            date_created: date,
+            company_id: companyId,
+            rating: rating
+        },
+        headers: { 'Content-type': 'application/json' }
+    })
+
+    if (review.ok) {
+        // document.location.replace('/company')
+    }
 })
