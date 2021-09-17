f63d77d03e0c2d9f70096bc7846a3db026560235
Rachel Chin <rtc145@gmail.com>
2021-09-17 18:14:38 -0400

    Stored sessions secret in .env and added config var in Heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
